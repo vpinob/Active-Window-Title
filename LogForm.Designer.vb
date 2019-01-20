e7dad1b382140fed9c208add7698cb1df6de9fc4
vpinob <46831704+vpinob@users.noreply.github.com>
2019-01-20 22:31:28 +0100

    Add files via upload

---
+++ LogForm.Designer.vb
@@ -0,0 +1,69 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class LogForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.BackColor = System.Drawing.Color.White
+        Me.TextBox1.Location = New System.Drawing.Point(6, 6)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(570, 246)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 5000
+        '
+        'LogForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(584, 261)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Enabled = False
+        Me.Location = New System.Drawing.Point(200, 200)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(600, 300)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(600, 300)
+        Me.Name = "LogForm"
+        Me.Text = "Windows Logg"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Timer1 As Timer
+    Private WithEvents TextBox1 As TextBox
+End Class
---
+++ LogForm.vb
@@ -0,0 +1,48 @@
+﻿Imports System
+Imports System.IO
+Imports System.Text
+
+Public Class LogForm
+
+    Private Declare Function GetForegroundWindow Lib "user32.dll" () As Int32
+    Private Declare Function GetWindowText Lib "user32.dll" Alias "GetWindowTextA" (ByVal hwnd As Int32, ByVal lpString As String, ByVal cch As Int32) As Int32
+
+    Private Function GetActiveWindowTitle() As String
+
+        Dim MyStr As String
+
+        MyStr = New String(Chr(0), 100)
+        GetWindowText(GetForegroundWindow, MyStr, 100)
+        MyStr = MyStr.Substring(0, InStr(MyStr, Chr(0)) - 1)
+
+        Return MyStr
+
+    End Function
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+        TextBox1.Text = TextBox1.Text + vbNewLine & My.Computer.Clock.LocalTime + " - " & GetActiveWindowTitle()
+
+    End Sub
+
+    Private Sub LogForm_Closing(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles MyBase.Closing
+
+        Dim path As String = "C:\Temp\File.txt"
+
+        If Not File.Exists(path) Then
+            My.Computer.FileSystem.CreateDirectory("C:\Temp")
+
+            Using sw As StreamWriter = File.CreateText(path)
+                sw.WriteLine(TextBox1.Text)
+            End Using
+        Else
+            Using sw As StreamWriter = File.AppendText(path)
+                sw.WriteLine(TextBox1.Text)
+            End Using
+        End If
+    End Sub
+
+    Private Sub LogForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
