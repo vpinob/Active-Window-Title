52ecb51b84185d5cea944607771d02d282b8de39
vpinob <46831704+vpinob@users.noreply.github.com>
2019-01-22 08:44:46 +0100

    Update LogForm.vb

--- LogForm.vb
+++ LogForm.vb
@@ -3,46 +3,49 @@ Imports System.IO
 Imports System.Text
 
 Public Class LogForm
-
+    
     Private Declare Function GetForegroundWindow Lib "user32.dll" () As Int32
     Private Declare Function GetWindowText Lib "user32.dll" Alias "GetWindowTextA" (ByVal hwnd As Int32, ByVal lpString As String, ByVal cch As Int32) As Int32
+            
+            'Get the active window title
+            Private Function GetActiveWindowTitle() As String
+                Dim StringTitle As String
 
-    Private Function GetActiveWindowTitle() As String
+                StringTitle = New String(Chr(0), 100)
+                GetWindowText(GetForegroundWindow, StringTitle, 100)
+                StringTitle = StringTitle.Substring(0, InStr(StringTitle, Chr(0)) - 1)
 
-        Dim MyStr As String
+                Return StringTitle
 
-        MyStr = New String(Chr(0), 100)
-        GetWindowText(GetForegroundWindow, MyStr, 100)
-        MyStr = MyStr.Substring(0, InStr(MyStr, Chr(0)) - 1)
+            End Function
+            
+            'Set the window title into TextBox1 each 5 seconds.
+            
+            Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
 
-        Return MyStr
+                TextBox1.Text = TextBox1.Text + vbNewLine & My.Computer.Clock.LocalTime + " - " & GetActiveWindowTitle()
 
-    End Function
+            End Sub
+            
+            
+            
+            Private Sub LogForm_Closing(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles MyBase.Closing
 
-    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
-
-        TextBox1.Text = TextBox1.Text + vbNewLine & My.Computer.Clock.LocalTime + " - " & GetActiveWindowTitle()
-
-    End Sub
-
-    Private Sub LogForm_Closing(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles MyBase.Closing
-
-        Dim path As String = "C:\Temp\File.txt"
-
-        If Not File.Exists(path) Then
-            My.Computer.FileSystem.CreateDirectory("C:\Temp")
-
-            Using sw As StreamWriter = File.CreateText(path)
-                sw.WriteLine(TextBox1.Text)
-            End Using
-        Else
-            Using sw As StreamWriter = File.AppendText(path)
-                sw.WriteLine(TextBox1.Text)
-            End Using
-        End If
-    End Sub
-
-    Private Sub LogForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
+                Dim path As String = "C:\Temp\File.txt"
+                
+                'Ones the form Closes, create a new directory "C:\Temp" if it does not exist.
+                If Not File.Exists(path) Then
+                    My.Computer.FileSystem.CreateDirectory("C:\Temp")
+                    
+                    'Writes all lines from TextBox1 into the file "File.txt".
+                    Using sw As StreamWriter = File.CreateText(path)
+                        sw.WriteLine(TextBox1.Text)
+                    End Using
+                Else
+            'Append the new lines into the the "File.txt"
+                    Using sw As StreamWriter = File.AppendText(path)
+                        sw.WriteLine(TextBox1.Text)
+                    End Using
+                End If
+            End Sub
 End Class
